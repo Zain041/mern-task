767808f810b43079b6e7c8f4265321cf8fd8daba
Muhammad Zain <zaingulzar143gmail.com>
2020-12-22 15:26:24 +0500

    finish

---
+++ client/build/precache-manifest.6962295399755e49c4d011e19e8080ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6d8232972643e06535ec0ff9cac802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dbce2468915895fc5af",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5fbb2de4005713afbfb7",
+    "url": "/static/css/main.50a0fd3d.chunk.css"
+  },
+  {
+    "revision": "5dbce2468915895fc5af",
+    "url": "/static/js/2.527acbf8.chunk.js"
+  },
+  {
+    "revision": "5fbb2de4005713afbfb7",
+    "url": "/static/js/main.af12dde2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
